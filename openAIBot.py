2c6fcb23d2a5e703386d5aa5f070a0898ac07d29
Arijit Das <arijit1080@gmail.com>
2023-09-15 03:44:50 +0530

    Add files via upload

---
+++ openAIBot.py
@@ -0,0 +1,58 @@
+import speech_recognition as sr
+import pyttsx3
+import openai
+
+#Initializing pyttsx3
+listening = True
+engine = pyttsx3.init()
+
+#Set your openai api key and customizing the chatgpt role
+openai.api_key = "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"
+messages = [{"role": "system", "content": "Your name is raspberry and you help regarding all the matters"}]
+
+#Customizing The output voice
+voices = engine.getProperty('voices')
+
+rate = engine.getProperty('rate')
+volume = engine.getProperty('volume')
+
+
+def get_response(user_input):
+    messages.append({"role": "user", "content": user_input})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+    return ChatGPT_reply
+
+
+while listening:
+    with sr.Microphone() as source:
+        recognizer = sr.Recognizer()
+        recognizer.adjust_for_ambient_noise(source)
+        recognizer.dynamic_energy_threshold = 3000
+
+        try:
+            print("Listening...")
+            audio = recognizer.listen(source, timeout=5.0)
+            response = recognizer.recognize_google(audio)
+            print(response)
+           
+            if "hello raspberry" in response.lower():
+           
+                response_from_openai = get_response(response)
+                engine.setProperty('rate', 105)
+                engine.setProperty('volume', volume)
+                engine.setProperty('voice', 'en-us')
+                engine.say(response_from_openai)
+                engine.runAndWait()
+               
+           
+               
+            else:
+                print("Didn't recognize 'hey chatgpt'.")
+           
+        except sr.UnknownValueError:
+            print("Didn't recognize anything.")
